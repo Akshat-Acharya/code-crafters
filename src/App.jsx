1fb0251df38c31ac8e247008426c80fa392bca80
Akshat-Acharya <akshat.acharya27@gmail.com>
2025-08-17 15:37:03 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from "./components/common/Navbar";
 import OpenRoute from "./components/core/Auth/OpenRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
-import VerifyEmail from "./pages/VerifyEmail";
+import VerifyEmail from "./pages/VerifyEmail.jsx";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import MyProfile from "./components/core/dashboard/MyProfile";
